84657c26e36606d030a1b21459099184c447be33
Leila <damovand@yahoo.com>
2025-01-07 15:19:37 -0500

    final project take 1

--- emotion_detection.py
+++ emotion_detection.py
@@ -2,9 +2,9 @@
 
 '''
 
-import requests
+import requests, jsonify
 import json
-
+from flask import Flask, jsonify
 def emotion_detector(text_to_analyse):
     # URL of the emotion analysis service
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -17,4 +17,19 @@ def emotion_detector(text_to_analyse):
 
     # Sending a POST request to the emotion analysis API
     response = requests.post(url, json=myobj, headers=header)
-    return response.text  # Return the response text from the API
+    return (response.text)
+    '''
+    data = response.json()
+    emotion_predictor = data.get('emotionPredictions')
+    
+    parsed_emotions = []
+    for emotion in emotion_predictor:
+        parsed_emotions.append({emotion})
+    '''
+    return jsonify(emotion_predictor)  # Return the response text from the API
+    
+        # Parsing the JSON response from the API
+   # formatted_response = json.loads(response.text)
+
+    #  I AM STUCK!
+    
